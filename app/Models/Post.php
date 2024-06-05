22de33c1611ca8e58306545def6bdae105f5b7c8
dantechdevs <damnngwasi@gmail.com>
2024-06-05 17:33:29 +0300

    updated

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
     use HasFactory;
+     protected $table = 'posts';
+     protected $fillable = ['id' ,'title','body'];
+
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(user::class)->withDefault('Admin User');
+    }
+
+    public function category():BelongsTo
+    {
+        return $this->belongsTo(category::class);
+    }
+    public function tags():BelongsToMany
+    {
+        return $this->belongsToMany(tag::class, 'post_tag');
+    }
 }
