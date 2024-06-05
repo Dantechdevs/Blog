845d39ac2a0bbc9da6dc30490d216c0d66263a4f
dantechdevs <damnngwasi@gmail.com>
2024-06-05 22:34:00 +0300

    migrations

--- database/migrations/2024_06_05_060638_create_posts_table.php
+++ database/migrations/2024_06_05_060638_create_posts_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->longText('body');
+            $table->text('meta_description')->nullable();
+            $table->timestamp('published_at')->nullable();
+            $table->boolean('featured')->default(0);
+            $table->foreignId('author_id')->constraints('users');
             $table->timestamps();
+
         });
     }
 
