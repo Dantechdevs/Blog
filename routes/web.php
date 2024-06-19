079592ec246cd22d6d1427c38f0b4b76c400ab4d
dantechdevs <damnngwasi@gmail.com>
2024-06-19 20:51:01 +0300

    updated

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,23 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 //Home Route
 Route::get('/', [HomeController::class, 'index'])->name('home');
-
+Route::group(['middleware' => ['auth'] , 'prefix' => 'dashboard'], function() {
+    //Dashboard
+    Route::group(['prefix' => '', 'as' => 'dashboard'], function() {
+    Route::get('/', [DashboardController::class, 'index'])->name('index');
+    });
+    //Categories
+    Route::resource('categories', CategoryController::class);
+});
 Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    'auth:sanctum',config('jetstream.auth_session'),'verified',])->group(function () {
+    Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
 });
 
 //Posts
